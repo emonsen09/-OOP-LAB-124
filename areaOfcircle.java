d934e06fffb3008465c6506d2dbd42a812e663d5
Emon Sen <134966336+emonsen09@users.noreply.github.com>
2023-11-06 15:34:04 +0600

    Create areaOfcircle.java

---
+++ areaOfcircle.java
@@ -0,0 +1,14 @@
+package mycompany.oop1sttask;
+import java.util.Scanner;
+public class AreaofCircle {
+    
+    public static void main(String[]args){
+       Scanner input = new Scanner (System.in);
+       int radius;
+       double area; 
+       System.out.print("enter radius=");
+       radius = input.nextInt();
+       area = (float)3.1416*radius*radius;
+       System.out.print("area is = "+area);          
+}
+}
